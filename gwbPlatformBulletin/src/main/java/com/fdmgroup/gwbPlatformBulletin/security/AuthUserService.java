4e8a8c8818949e7b3900ee60e9b2601a13223ed6
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-05-07 15:44:54 +1000

    add security files to add integration to project

---
+++ gwbPlatformBulletin/src/main/java/com/fdmgroup/gwbPlatformBulletin/security/AuthUserService.java
@@ -0,0 +1,29 @@
+package com.fdm.userDemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fdm.userDemo.model.User;
+import com.fdm.userDemo.dal.*;
+
+@Service
+public class AuthUserService implements org.springframework.security.core.userdetails.UserDetailsService{
+	private UserRepository userRepo;
+
+	@Autowired
+	public AuthUserService(UserRepository userRepo) {
+		super();
+		this.userRepo = userRepo;
+	}
+
+	@Override
+	public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.userRepo.findByFirstName(username).orElseThrow(
+				()-> new UsernameNotFoundException(username));
+		return new AuthUser(user);
+	}
+	
+	
+
+}

4001b51478cab6d86ef53b4c032d2429847caf30
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-05-08 13:37:41 +1000

    change from Exception to RuntimeException to bypass issues with registerMember method not allowing us to save member dummy data

--- gwbPlatformBulletin/src/main/java/com/fdmgroup/gwbPlatformBulletin/exceptions/ConflictException.java
+++ gwbPlatformBulletin/src/main/java/com/fdmgroup/gwbPlatformBulletin/exceptions/ConflictException.java
@@ -1,8 +1,8 @@
 package com.fdmgroup.gwbPlatformBulletin.exceptions;
 
-public class ConflictException extends Exception {
+public class ConflictException extends RuntimeException {
 	
-	private static final long serialVersionUID = 3678121914181316925L;
+//	private static final long serialVersionUID = 3678121914181316925L;
 
 	public ConflictException(String message) {
 		super(message);
